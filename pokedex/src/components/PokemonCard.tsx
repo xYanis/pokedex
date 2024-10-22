113bb62acd8420d7d62801dcd07485a369d5aa21
xYanis <yanis.hortholary@gmail.com>
2024-10-22 09:55:48 +0200

    React 2

---
+++ pokedex/src/components/PokemonCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PokemonCard: React.FC = () => {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbasaur"
+      />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+};
+
+export default PokemonCard;
+
