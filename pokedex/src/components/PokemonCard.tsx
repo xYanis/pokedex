9e7139bfd193386130cb918fd211313d050d688c
xYanis <yanis.hortholary@gmail.com>
2024-10-22 10:24:23 +0200

    React 3

--- pokedex/src/components/PokemonCard.tsx
+++ pokedex/src/components/PokemonCard.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const PokemonCard: React.FC = () => {
+  const pokemon = pokemonList[1]; // Premier Pokémon du tableau
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {/* Afficher l'image seulement si imgSrc est défini, sinon afficher ??? */}
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption> {/* Nom du Pokémon */}
     </figure>
   );
 };
 
 export default PokemonCard;
-
